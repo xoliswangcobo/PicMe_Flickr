5abb6b5776b4804b361e2a0a657ed7be99c737cb
Xoliswa Ngcobo <xoliswangcobo@MacBook-Pro.local>
2017-02-22 22:17:36 +0200

    Create the other two viewcontrollers.

---
+++ picme/ViewControllers/DetailedPhotoViewController/DetailedPhotoViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailedPhotoViewController.h
+//  picme
+//
+//  Created by Xoliswa Ngcobo on 2017/02/22.
+//  Copyright © 2017 XoliswaNgcobo. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface DetailedPhotoViewController : BaseViewController
+
+@end
---
+++ picme/ViewControllers/DetailedPhotoViewController/DetailedPhotoViewController.m
@@ -0,0 +1,37 @@
+//
+//  DetailedPhotoViewController.m
+//  picme
+//
+//  Created by Xoliswa Ngcobo on 2017/02/22.
+//  Copyright © 2017 XoliswaNgcobo. All rights reserved.
+//
+
+#import "DetailedPhotoViewController.h"
+
+@interface DetailedPhotoViewController ()
+
+@end
+
+@implementation DetailedPhotoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

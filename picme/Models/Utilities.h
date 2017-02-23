e0f1caeadb57dccdd90d4caeebd6943b3a30960b
Xoliswa Ngcobo <xoliswangcobo@MacBook-Pro.local>
2017-02-23 22:16:41 +0200

    Added the utilities class files.

---
+++ picme/Models/Utilities.h
@@ -0,0 +1,13 @@
+//
+//  Utilities.h
+//  picme
+//
+//  Created by Xoliswa Ngcobo on 2017/02/23.
+//  Copyright © 2017 XoliswaNgcobo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Utilities : NSObject
+
+@end
